79d4cc89ee9f121caafc18cf0e8e9f9595ce038a
M.Ngugi <mngugi7@gmail.com>
2024-09-17 15:26:37 +0300

    Create chal.c

---
+++ CTF_Cookbook/chal.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char buffer1[16];
+    char buffer[16] = "xxxxxxxxxxxxxxx";
+
+    gets(buffer1);
+
+    if (strcmp(buffer2, impossible)== 0) {
+
+        puts("flag{win}");
+
+    } else {
+
+        puts("nope");
+    }
+
+
+
+}

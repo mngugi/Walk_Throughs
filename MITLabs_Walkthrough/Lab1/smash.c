a7db28437f90389fe7576e6c256c6608f80cae2c
P.M.Ngugi <mngugi7@hotmail.com>
2026-09-14 16:31:29 +0300

    MITLabs_Walkthrough/Lab1/smash

--- MITLabs_Walkthrough/Lab1/smash.c
+++ MITLabs_Walkthrough/Lab1/smash.c
@@ -1,1 +1,21 @@
 
+#include <stdio.h>
+#include <string.h>
+
+void vulnerable(void)
+{
+    char buffer[8];
+
+    strcpy(buffer, "AAAAAAAAAAAAAAAA");
+
+    printf("Buffer: %s\n", buffer);
+}
+
+int main(void)
+{
+    vulnerable();
+
+    printf("Program continued normally.\n");
+
+    return 0;
+}
